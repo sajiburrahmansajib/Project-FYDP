f17e385c71150fd6c1b29efed415686c1e0662f7
Md Imam Hossain <71822547+imamtuhin@users.noreply.github.com>
2022-02-27 18:56:03 +0600

    Update urls.py

--- implementation/E_health_consultation/E_health_consultation/urls.py
+++ implementation/E_health_consultation/E_health_consultation/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from health.views import *
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home,name="home"),
